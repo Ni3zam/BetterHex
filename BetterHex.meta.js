b01cb515bb699e08f2b24ab0e3bbd64a9a56689f
Logfro <moritziphone239@gmail.com>
2017-12-07 22:14:38 +0100

    Update BetterHex.meta.js

--- BetterHex.meta.js
+++ BetterHex.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better HEx by Logfro
 // @namespace    https://logfro.de/
-// @version      0.52
+// @version      0.53
 // @description  Better HEx adds useful functions to the legacy hacker experience
 // @author       Logfro
 // @match        https://legacy.hackerexperience.com/*
