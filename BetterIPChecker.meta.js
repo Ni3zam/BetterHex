855d8ce72562cdd0df040df811ae2e01ab84ac1c
Ni3zam <rahulniezam2002@gmail.com>
2017-12-31 00:23:55 +0800

    Create BetterIPChecker.meta.js

---
+++ BetterIPChecker.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         BetterIPChecker by Logfro
+// @namespace    https://logfro.de/
+// @version      1.0
+// @description  Better HEx adds useful functions to the legacy hacker experience
+// @author       Logfro
+// @match        https://legacy.hackerexperience.com/*
+// @updateURL    https://gitcdn.xyz/repo/Logfro/BetterHex/master/BetterIPChecker.meta.js
+// @downloadURL  https://gitcdn.xyz/repo/Logfro/BetterHex/master/BetterIPChecker.user.js
+// @grant        none
+// ==/UserScript==
